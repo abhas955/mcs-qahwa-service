a01e1422a77bdbb929970693b658ec3b1e363cf8
abdullahiahhmed <abhas955@gmail.com>
2026-03-04 13:40:06 -0500

    adding controller for qahwa service. closes #2

---
+++ src/main/java/ahmed/abdullahi/mcsqahwaservice/web/controller/QahwaController.java
@@ -0,0 +1,33 @@
+package ahmed.abdullahi.mcsqahwaservice.web.controller;
+
+
+import ahmed.abdullahi.mcsqahwaservice.web.model.QahwaDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/qahwa")
+public class QahwaController {
+
+    public ResponseEntity<QahwaDto> getQahwaById(@PathVariable("qahwaId") UUID qahwaId){
+        //todo impl
+        return new ResponseEntity<>(QahwaDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewQahwa(@RequestBody QahwaDto qahwaDto){
+
+        //todo impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{qahwaId}")
+    public ResponseEntity updateQahwaById(@PathVariable("qahwaId") UUID qahwaId, @RequestBody QahwaDto qahwaDto){
+        //todo impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+}
